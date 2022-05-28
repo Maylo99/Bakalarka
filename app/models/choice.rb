610d512421a45af997242f23443b34de71d7cdbb
Maylo99 <hajekmwork@gmail.com>
2022-05-28 20:43:26 +0200

    bug fix

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,5 +2,4 @@ class Choice < ApplicationRecord
   belongs_to :multiple_choice_question,optional: true
   validates :answer, presence: true
   validates :partialPoints, presence: true
-  validates :is_true, presence: true
 end
